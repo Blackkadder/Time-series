49e1a9d9737ae80b37802eac4c477ac5fa2f0ce7
Rob B <robinnep@gmail.com>
2017-03-20 17:37:21 -0500

    added imports

--- ARIMA with exogenous.py
+++ ARIMA with exogenous.py
@@ -1,11 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+
+#calculate mape
 def mape(y_true, y_pred): 
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
+#calculate rmse
 def rmse(y_true, y_pred):
     return np.sqrt(((y_pred - y_true) ** 2).mean())
 
-# temp_df = pd.DataFrame()    
-def state_reg(state):
+# main arima function    
+def arima_x(df1,df2):
     Y = np.log(df1)
     X = np.log(df2)
     results = sm.tsa.ARIMA(Y, order = (2,1,1), exog = X).fit()
